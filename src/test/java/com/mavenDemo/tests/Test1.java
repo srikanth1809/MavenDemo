f06ac9e6d7dd4eb5cea81ffb3ffe1fdf3a19ffa2
SRICHARAN <SRICHARAN@192.168.0.5>
2018-09-27 09:58:36 +0530

    A Framework 1.0

---
+++ src/test/java/com/mavenDemo/tests/Test1.java
@@ -0,0 +1,14 @@
+package com.mavenDemo.tests;
+
+import org.testng.Reporter;
+
+public class Test1
+{
+	public void Selenium()
+	{
+		Reporter.log("Selenium is my Life",true);	
+		Reporter.log("Trying to improve on Java,true");
+		Reporter.log("Hard work,true");
+	}
+
+}
